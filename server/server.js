5f324b1e2308cb508b8c628a9610df1315db0e69
Roshan Gupta <rg9885468@gmail.com>
2025-07-18 16:54:13 +0530

    Fix: Suppressed react-datepicker source map warnings

--- server/server.js
+++ server/server.js
@@ -9,11 +9,20 @@ const paymentRoutes = require('./src/routes/paymentRoutes');
 const cors = require('cors');
 const app = express();
 
+const allowedOrigins = process.env.CLIENT_ENDPOINT?.split(',') || [];
+
 app.use(cors({
-    origin: process.env.CLIENT_URL,
+    origin: function (origin, callback) {
+        // Allow requests with no origin (like mobile apps or curl)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.includes(origin)) {
+            return callback(null, true);
+        } else {
+            return callback(new Error('Not allowed by CORS'));
+        }
+    },
     credentials: true
 }));
-
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB Connected'))
     .catch((error) => console.log(error));
@@ -33,6 +42,6 @@ app.listen(PORT, (error) => {
     if (error) {
         console.log('Error starting the server: ', error);
     } else {
-        console.log(`Server is running at http://localhost:${PORT}`);
+        console.log('Server is running at http://localhost:${PORT}');
     }
 });
